09ddd2adbf3daad885444585d4b1378050bed019
Ian Odundo <thisian13@gmail.com>
2020-10-13 23:02:37 +0300

    mpi sample

---
+++ mpi.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+num_p = comm.Get_size()
+
+
+if rank != 0:
+    comm.send("Hello From: " + str(rank), dest=0)
+else:
+    for pid in range(1, num_p):
+        print("Message: ", comm.recv(source=pid))
